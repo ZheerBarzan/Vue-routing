12f17b118e69c67354c862b17183daa394305496
ZheerBarzan <zheerbarzan85@outlook.com>
2023-10-04 18:34:57 +0300

    now every route has its own vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter , createWebHistory} from "vue-router";
 import Home from "../views/Home.vue";
-import About from "../views/About.vue"
+import About from "../views/About.vue";
+import CarView from "../views/CarView.vue"
 
 const router = createRouter({
 history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ routes:[
         path: '/about',
         name:"about",
         component: About
+    },
+    {
+        path: "/cars/:id",
+        name: "car",
+        component: CarView,
     }
 ]
 })
---
+++ src/views/CarView.vue
@@ -0,0 +1,6 @@
+<template>
+
+    <div>
+        <h1>car view</h1>
+    </div>
+</template>
