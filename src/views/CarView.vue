3c8815459fcbe4f1310bc04688f573e78a3f1765
ZheerBarzan <zheerbarzan85@outlook.com>
2023-10-05 23:34:05 +0300

    now every page is corespondent with its value

--- src/views/CarView.vue
+++ src/views/CarView.vue
@@ -1,6 +1,23 @@
+ <script setup>
+import {useRoute} from "vue-router"
+import cars from "../data/cars.json"
+
+
+
+const route = useRoute()
+
+const car = cars.find(c => c.id === parseInt(route.params.id))
+
+
+</script>
+
+
+
+
 <template>
 
     <div>
         <h1>car view</h1>
+        {{ car }}
     </div>
 </template>
